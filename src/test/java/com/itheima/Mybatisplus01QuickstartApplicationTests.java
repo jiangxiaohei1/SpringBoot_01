d48c20b93208d544aaf9418b373b07713bd0ee15
xiaohei <1685092708@qq.com>
2022-10-07 19:33:24 +0800

    master commit1

--- src/test/java/com/itheima/Mybatisplus01QuickstartApplicationTests.java
+++ src/test/java/com/itheima/Mybatisplus01QuickstartApplicationTests.java
@@ -24,6 +24,7 @@ class Mybatisplus01QuickstartApplicationTests {
         System.out.println(users);
         System.out.println("nihaoa");
         System.out.println("123456789");
+        System.out.println("master test");
 
     }
 
