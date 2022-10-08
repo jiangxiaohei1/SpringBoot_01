b9199bd9919d73360ea47dddfe6002ec7be0841a
jiangxiaohei1 <115155729+jiangxiaohei1@users.noreply.github.com>
2022-10-08 18:56:33 +0800

    pull test

--- src/test/java/com/itheima/Mybatisplus01QuickstartApplicationTests.java
+++ src/test/java/com/itheima/Mybatisplus01QuickstartApplicationTests.java
@@ -27,6 +27,7 @@ class Mybatisplus01QuickstartApplicationTests {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 
